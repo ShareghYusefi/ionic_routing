af02d13c63801a45e47cdb416c9fbfa68075ce94
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2024-12-05 17:35:47 -0700

    navigating with tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,31 +3,38 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    // Lazy-loading modules: This is done so that the home page is loaded only when the user navigates to it.
-    loadChildren: () =>
-      import('./home/home.module').then((m) => m.HomePageModule),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'about',
-    loadChildren: () =>
-      import('./about/about.module').then((m) => m.AboutPageModule),
-  },
-  {
-    path: 'contact/:id',
-    loadChildren: () =>
-      import('./contact/contact.module').then((m) => m.ContactPageModule),
-  },
-  {
-    path: 'contact',
-    loadChildren: () =>
-      import('./contact/contact.module').then((m) => m.ContactPageModule),
-  },
+    path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  }
+  // {
+  //   path: 'home',
+  //   // Lazy-loading modules: This is done so that the home page is loaded only when the user navigates to it.
+  //   loadChildren: () =>
+  //     import('./home/home.module').then((m) => m.HomePageModule),
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full',
+  // },
+  // {
+  //   path: 'about',
+  //   loadChildren: () =>
+  //     import('./about/about.module').then((m) => m.AboutPageModule),
+  // },
+  // {
+  //   path: 'contact/:id',
+  //   loadChildren: () =>
+  //     import('./contact/contact.module').then((m) => m.ContactPageModule),
+  // },
+  // {
+  //   path: 'contact',
+  //   loadChildren: () =>
+  //     import('./contact/contact.module').then((m) => m.ContactPageModule),
+  // },
+  // {
+  //   path: 'tabs',
+  //   loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  // },
 ];
 
 @NgModule({
