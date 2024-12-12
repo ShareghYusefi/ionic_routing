945338f081cb2d307927c94db2574f0871f0fa42
Sharegh Yusefi <48734749+ShareghYusefi@users.noreply.github.com>
2024-12-11 17:44:04 -0700

    infinite scroll list

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { InfiniteScrollCustomEvent, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-about',
@@ -10,8 +10,23 @@ import { NavController } from '@ionic/angular';
 export class AboutPage implements OnInit {
   // ! means that the variable will be initialized later
   email!: string;
+  items: string[] = [];
   // We can use NavController to define methods for navigation
   constructor(private navCtrl: NavController, private route: ActivatedRoute) {}
+
+  private generateItems() {
+    const count = this.items.length + 1;
+    for (let i = 0; i < 50; i++) {
+      this.items.push(`Item ${count + i}`);
+    }
+  }
+
+  onIonInfinite(ev: Event) {
+    this.generateItems();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
 
   // navigate to about page
   navigateBackHome() {
@@ -19,6 +34,8 @@ export class AboutPage implements OnInit {
   }
 
   ngOnInit() {
+    this.generateItems();
+
     this.route.queryParams.subscribe((params) => {
       this.email = params['email'];
     });
